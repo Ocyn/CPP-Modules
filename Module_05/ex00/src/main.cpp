72db9e4d6cbfe66d88f489645b11ce075f42bf9a
Ocyn <niko.ocyn@gmail.com>
2024-07-18 13:31:36 +0200

    CPP5: first time using try catch im  still learning

--- Module_05/ex00/src/main.cpp
+++ Module_05/ex00/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcuzin <jcuzin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ocyn <ocyn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 09:31:53 by jcuzin            #+#    #+#             */
-/*   Updated: 2024/07/17 17:38:57 by jcuzin           ###   ########.fr       */
+/*   Updated: 2024/07/18 13:29:28 by ocyn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #define _NB 10
 
-void	Test( Bureaucrat Peal )
+void	Test( Bureaucrat &Peal )
 {
 	std::cout << Peal.getName() << ":" << Peal.getGrade() << std::endl;
 	std::cout << std::endl;
@@ -28,5 +28,18 @@ int main()
 
 	Bureaucrat Peal("Michel", 99);
 
+	Bureaucrat	*Yo;
+
+	try
+	{
+		Yo = new Bureaucrat("Jojo", 8);
+		Test(*Yo);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	Test(Peal);
+
+	delete Yo;
 }
