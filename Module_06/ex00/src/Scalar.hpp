53ede202fcdd6b348a2542742d4e8b98b5ebf9a9
Ocyn <niko.ocyn@gmail.com>
2024-08-02 13:20:41 +0200

    CPP06 ex00 in progress: lexical cast choosen, but can't figure out how to display interpretations results...Very confused exercise subject...

--- Module_06/ex00/src/Scalar.hpp
+++ Module_06/ex00/src/Scalar.hpp
@@ -6,7 +6,7 @@
 /*   By: ocyn <ocyn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 12:24:34 by jcuzin            #+#    #+#             */
-/*   Updated: 2024/07/31 17:47:12 by ocyn             ###   ########.fr       */
+/*   Updated: 2024/08/02 13:05:48 by ocyn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,17 @@
 # include <cstdlib>
 # include <cstring>
 # include <stdexcept>
+# include <boost/lexical_cast.hpp>
 
 typedef std::string string;
 
 class	Scalar {
 
-	public:
+	private:
 		Scalar		(void);
 		Scalar		(const Scalar &Sample);
-		virtual	~Scalar		(void);
+	public:
+		~Scalar		(void);
 
 		Scalar&		operator=(const Scalar &Sample);
 
@@ -35,11 +37,18 @@ class	Scalar {
 		static void	convert(string Literal);
 
 	public:
-		class	Fail: public std::exception {
+		class	Impossible: public boost::bad_lexical_cast {
 			public:
 				virtual const char * what() const throw()
 				{
-					return ("Operation failed");
+					return ("Impossible");
+				}
+		};
+		class	NotDisplayableInput: public std::exception {
+			public:
+				virtual const char * what() const throw()
+				{
+					return ("Not displayable input");
 				}
 		};
 };
--- Module_06/ex00/src/main.cpp
+++ Module_06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ocyn <ocyn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 09:31:53 by jcuzin            #+#    #+#             */
-/*   Updated: 2024/07/31 17:47:30 by ocyn             ###   ########.fr       */
+/*   Updated: 2024/08/02 13:11:05 by ocyn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,35 @@
 
 #define _NB 10
 
-int main()
-{
-	std::cout << "\nTEST 1" << std::endl;
-	/*	TEST 1	*/
+// void	_display(string Type, string Data)
+// {
+// 	int		Entier = 42;
+// 	char	Lettre = 'A';
+// 	float	Decimale = 42.42;
+// 	double	SuperDecimale = 42.4242;
 
-	
+// 	std::cout << Type << ": ";
+// 	std::cout << Data;
+// 	std::cout << std::endl;
+// }
+
+int main(int ac, char **av)
+{
+	if (ac != 2) {
+		std::cerr << "\n  Invalid argument format.\nOne argument at execution espected." << std::endl;
+		return (1);
+	}
+	std::cout << "\n________Convert\n" << std::endl;
+
+	string	In = av[1];
+	try
+	{
+		Scalar::convert(In);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	(void)av;
 	return (0);
 }
